3747c672357013d5fa1b68a4dad888998b0f5cdb
Kevin <mrhallonline@gmail.com>
2019-12-25 00:05:47 -0500

    add html

--- script1.py
+++ script1.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Website content goes here!"
+    return render_template("index.html")
+
+@app.route('/about/')
+def about():
+    return "This is the about page"
 
 if __name__ == "__main__":
     app.run(debug=True)
